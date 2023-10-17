e9a0f0dad03ab81a6706c40805ba45a3f4d38a95
Piranhajama <31992385+Pirahnajama@users.noreply.github.com>
2023-10-17 10:21:34 +0200

    Initializing project

---
+++ kata.chiffre.romain/src/kata/chiffre/romain/Main.java
@@ -0,0 +1,29 @@
+package kata.chiffre.romain;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println(numberToRomanDigit(0));
+		//System.out.println(numberToRomanDigit(1));
+		//System.out.println(numberToRomanDigit(2));
+		//System.out.println(numberToRomanDigit(3));
+		//System.out.println(numberToRomanDigit(5));
+		//System.out.println(numberToRomanDigit(4));
+		//System.out.println(numberToRomanDigit(7));
+		//System.out.println(numberToRomanDigit(10));
+		//System.out.println(numberToRomanDigit(9));
+		//System.out.println(numberToRomanDigit(12));
+		//System.out.println(numberToRomanDigit(14));
+		//System.out.println(numberToRomanDigit(17));
+		//System.out.println(numberToRomanDigit(20));
+
+	}
+	
+	private static String numberToRomanDigit(int i) {
+		//TODO transform the input number into his equivalent in Roman Digit
+		
+		return null;
+	}
+
+}
